8b543f178997e44d6ed6603cdddd604ac1a7efa3
Ethan Massey <ejm4aa@virginia.edu>
2021-12-01 12:04:03 -0500

    added day 1 part 2

--- day1/depths.py
+++ day1/depths.py
@@ -1,4 +1,5 @@
 
+''' PART 1 '''
 def get_increasing_depths(datafile):
 
 	depths = open(datafile, "r").readlines()
@@ -17,3 +18,38 @@ def get_increasing_depths(datafile):
 
 
 print(get_increasing_depths("depths.txt"))
+
+###################################################################
+''' PART 2 '''
+
+# window of size 3
+def get_increasing_window_depths(datafile):
+
+	depths = open(datafile, "r").readlines()
+	# format lines because I'm tired of typing so much
+	for i in range(0, len(depths)):
+		depths[i] = int(depths[i].strip('\n'))
+
+	# total of measurements > than previous measurement
+	total = 0
+	# get first window
+	window = [depths[0], depths[1], depths[2]]
+	previous_sum = sum(window)
+
+	for i in range(3, len(depths)):
+		# shift
+		window[0] = window[1]
+		window[1] = window[2]
+		window[2] = depths[i]
+
+		if(sum(window) > previous_sum):
+			total += 1
+
+		previous_sum = sum(window)
+
+	return total
+
+print(get_increasing_window_depths("depths.txt"))
+
+
+
