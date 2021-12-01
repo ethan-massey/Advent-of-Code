fe41128bc6696c5d0b3a1ff4bcc606a69303b183
Ethan Massey <ejm4aa@virginia.edu>
2021-12-01 11:01:29 -0500

    first commit

---
+++ day1/day1.py
@@ -0,0 +1,19 @@
+
+def get_decreasing_depths(datafile):
+
+	depths = open(datafile, "r").readlines()
+
+	# total of measurements < than previous measurement
+	total = 0
+
+	for i in range(1, len(depths)):
+		format_depth = int(depths[i].strip('\n'))
+		prev_format_depth = int(depths[i-1].strip('\n'))
+		# if measurement < previous measurement
+		if format_depth < prev_format_depth:
+			total += 1
+
+	return total
+
+
+print(get_decreasing_depths("depths.txt"))
