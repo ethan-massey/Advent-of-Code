7531aa54bf740336658873503d80074fd6d3192f
Ethan Massey <ejm4aa@virginia.edu>
2021-12-01 11:14:00 -0500

    changed decreasing to decreasing

--- day1/day1.py
+++ day1/day1.py
@@ -1,19 +1,19 @@
 
-def get_decreasing_depths(datafile):
+def get_increasing_depths(datafile):
 
 	depths = open(datafile, "r").readlines()
 
-	# total of measurements < than previous measurement
+	# total of measurements > than previous measurement
 	total = 0
 
 	for i in range(1, len(depths)):
 		format_depth = int(depths[i].strip('\n'))
 		prev_format_depth = int(depths[i-1].strip('\n'))
-		# if measurement < previous measurement
-		if format_depth < prev_format_depth:
+		# if measurement > previous measurement
+		if format_depth > prev_format_depth:
 			total += 1
 
 	return total
 
 
-print(get_decreasing_depths("depths.txt"))
+print(get_increasing_depths("depths.txt"))
