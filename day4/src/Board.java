ae20169159906fefad945de7ca6c8e020505b687
Ethan Massey <ejm4aa@virginia.edu>
2021-12-08 17:51:00 -0500

    day4 part 1 done

--- day4/src/Board.java
+++ day4/src/Board.java
@@ -1,11 +1,13 @@
+import java.util.ArrayList;
 
 class Board {
 
-    private Number numbers[][];
+    private Number[][] numbers;
+    private boolean isWinningBoard;
 
     // takes array of 25 nums and puts them in board
     public Board(int[] nums){
-
+        isWinningBoard = false;
         numbers = makeBoard(nums);
     }
 
@@ -19,6 +21,70 @@ class Board {
         }
 
         return retNums;
+    }
+
+    // mark number and check if winning board
+    public boolean markAndCheck(int i){
+        for(int r = 0; r < 5; r++){
+            for(int c = 0; c < 5; c++){
+                if(numbers[r][c].getValue() == i){
+                    numbers[r][c].setMarked(true);
+                    this.isWinningBoard = checkWinning();
+                }
+            }
+        }
+        return isWinningBoard;
+    }
+
+    private boolean checkWinning(){
+        int rowMarks = 0;
+        int colMarks = 0;
+
+        for(int r = 0; r < 5; r++){
+            for(int c = 0; c < 5; c++){
+                if(numbers[r][c].isMarked()){
+                    rowMarks++;
+                    if(rowMarks == 5){ return true; }
+                }
+            }
+            boolean f;
+            rowMarks = 0;
+        }
+
+        for(int c = 0; c < 5; c++){
+            for(int r = 0; r < 5; r++){
+                if(numbers[r][c].isMarked()){
+                    colMarks++;
+                    if(colMarks == 5){ return true; }
+                }
+            }
+            colMarks = 0;
+        }
+        return false;
+    }
+
+    private ArrayList<Integer> getUnmarked(){
+        ArrayList<Integer> ret = new ArrayList<Integer>();
+
+        for(int r = 0; r < 5; r++){
+            for(int c = 0; c < 5; c++){
+                if(!numbers[r][c].isMarked()){
+                    ret.add(numbers[r][c].getValue());
+                }
+            }
+        }
+        return ret;
+    }
+
+    public int getSumUnmarked(){
+        int sum = 0;
+        ArrayList<Integer> um = this.getUnmarked();
+
+        for(Integer i : um){
+            sum += i;
+        }
+
+        return sum;
     }
 
     @Override
--- day4/src/Main.java
+++ day4/src/Main.java
@@ -1,7 +1,19 @@
+
 public class Main {
     public static void main(String[] args) {
         DataFormatter d = new DataFormatter("data.txt");
 
+        Board[] boards = d.getBoards();
+        int[] callerNums = d.getCallerList();
 
+        for(int i : callerNums){
+            for(Board b : boards){
+                if(b.markAndCheck(i)){
+                    int sumUnmarked = b.getSumUnmarked();
+                    System.out.println(sumUnmarked * i);
+                    System.exit(0);
+                }
+            }
+        }
     }
 }
