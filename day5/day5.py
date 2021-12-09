16e51d146e558d8d7328c2e62b4cba8991e93f07
Ethan Massey <ejm4aa@virginia.edu>
2021-12-08 22:57:20 -0500

    started day 5

---
+++ day5/day5.py
@@ -0,0 +1,48 @@
+
+#format data
+lines = open("data.txt", "r").readlines()
+max_x = 0
+max_y = 0
+
+command_list = []
+for i in range(0, len(lines)):
+	command_list.append(lines[i].strip().split(' -> '))
+
+for i in range(0, len(command_list)):
+	for n in range(0, len(command_list[i])):
+		command_list[i][n] = command_list[i][n].split(',')
+		command_list[i][n][0] = int(command_list[i][n][0])
+		command_list[i][n][1] = int(command_list[i][n][1])
+		max_x = (max_x, command_list[i][n][0])[command_list[i][n][0] > max_x]
+		max_y = (max_y, command_list[i][n][1])[command_list[i][n][1] > max_y]
+
+# command_list format:
+# [command][command position][command position x or y]
+
+rows, cols = (max_x, max_y)
+graph = [['.' for i in range(cols)] for j in range(rows)]
+
+# adds a layer to the graph position.
+def add_layer(x_pos, y_pos):
+	cur_val = graph[x_pos][y_pos]
+	if cur_val == '.':
+		graph[x_pos][y_pos] = 1
+	else:
+		graph[x_pos][y_pos] = cur_val + 1
+
+# takes line end points and returns a list of coords to add a layer to
+def get_straight_line(x1, y1, x2, y2):
+	ret = []
+	
+	# example input: 0,1,0,5 -> [(0,1), (0,2), (0,3), (0,4), (0,5)]
+	return ret
+
+
+# print(get_line(0,2,6,2))
+
+
+
+
+
+
+
