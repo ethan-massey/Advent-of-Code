92dbeeddb02c8a14df044a7a11a621d26c7921eb
Ethan Massey <ejm4aa@virginia.edu>
2024-12-14 20:20:00 -0500

    done with day 2

--- 2024/day2/day2.py
+++ 2024/day2/day2.py
@@ -5,20 +5,32 @@ for line in lines:
     report = [int(i) for i in line.split(" ")]
     reports.append(report)
 
-# part 1
 
-
-def is_safely_increasing(report) -> bool:
+def is_safely_increasing(report, is_second_attempt) -> bool:
     for i in range(1, len(report)):
         if report[i] <= report[i - 1] or not is_safe_jump(report[i], report[i - 1]):
-            return False
+            if is_second_attempt:
+                return False
+            else:
+                fixed_a = remove_index_from_report(report, i)
+                fixed_b = remove_index_from_report(report, i - 1)
+                return is_safely_increasing(fixed_a, True) or is_safely_increasing(
+                    fixed_b, True
+                )
     return True
 
 
-def is_safely_decreasing(report) -> bool:
+def is_safely_decreasing(report, is_second_attempt) -> bool:
     for i in range(1, len(report)):
         if report[i] >= report[i - 1] or not is_safe_jump(report[i], report[i - 1]):
-            return False
+            if is_second_attempt:
+                return False
+            else:
+                fixed_a = remove_index_from_report(report, i)
+                fixed_b = remove_index_from_report(report, i - 1)
+                return is_safely_decreasing(fixed_a, True) or is_safely_decreasing(
+                    fixed_b, True
+                )
     return True
 
 
@@ -27,12 +39,22 @@ def is_safe_jump(num1, num2) -> bool:
     return 1 <= diff <= 3
 
 
+def remove_index_from_report(report, index) -> list:
+    return report[:index] + report[index + 1 :]
+
+
+# part 1
 safe_reports = []
 for report in reports:
-    if is_safely_decreasing(report) or is_safely_increasing(report):
+    if is_safely_decreasing(report, True) or is_safely_increasing(report, True):
         safe_reports.append(report)
 
 print(len(safe_reports))
 
 # part 2
+safe_reports = []
+for report in reports:
+    if is_safely_decreasing(report, False) or is_safely_increasing(report, False):
+        safe_reports.append(report)
 
+print(len(safe_reports))
