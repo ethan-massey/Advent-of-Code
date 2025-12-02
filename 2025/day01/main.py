68382e89b7967b7b2a972b0efaeb821843c92503
ethan-massey <44252355+ethan-massey@users.noreply.github.com>
2025-12-02 14:14:27 -0500

    Optimize day 1 part 2

--- 2025/day01/main.py
+++ 2025/day01/main.py
@@ -3,11 +3,13 @@ def get_rotation_list():
     rotations = [l.strip() for l in f.readlines()]
     return rotations
 
+
 class Dial:
 
     def __init__(self):
         self.dial_location = 50
         self.times_hit_zero = 0
+        self.times_passed_zero = 0
 
     def process_rotation_list(self, rotations):
         for rotation in rotations:
@@ -15,6 +17,17 @@ class Dial:
 
     def rotate_dial(self, addend: str):
         int_addend = self.translate_addend(addend)
+
+        # if addend >= 100 lets just process the extra turns here
+        if abs(int_addend) >= 100:
+            self.times_passed_zero += abs(int_addend) // 100
+            int_addend = abs(int_addend) % 100 if int_addend >= 0 else - (abs(int_addend) % 100)
+
+        # then process the remaining movement
+        if self.dial_location != 0:
+            if self.dial_location + int_addend > 99 or self.dial_location + int_addend <= 0:
+                self.times_passed_zero += 1
+
         self.dial_location = (self.dial_location + int_addend) % 100
         if self.dial_location == 0:
             self.times_hit_zero += 1
@@ -30,3 +43,4 @@ dial = Dial()
 rotations = get_rotation_list()
 dial.process_rotation_list(rotations)
 print(dial.times_hit_zero)
+print(dial.times_passed_zero)
