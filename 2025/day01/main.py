213dab8a1a70de9c2642e71d40e1d9d995ad5286
Ethan Massey <ejm4aa@virginia.edu>
2025-12-01 23:25:57 -0500

    day1 part 1

---
+++ 2025/day01/main.py
@@ -0,0 +1,32 @@
+def get_rotation_list():
+    f = open('day01.txt', 'r')
+    rotations = [l.strip() for l in f.readlines()]
+    return rotations
+
+class Dial:
+
+    def __init__(self):
+        self.dial_location = 50
+        self.times_hit_zero = 0
+
+    def process_rotation_list(self, rotations):
+        for rotation in rotations:
+            self.rotate_dial(rotation)
+
+    def rotate_dial(self, addend: str):
+        int_addend = self.translate_addend(addend)
+        self.dial_location = (self.dial_location + int_addend) % 100
+        if self.dial_location == 0:
+            self.times_hit_zero += 1
+
+    def translate_addend(self, addend: str) -> int:
+        num = int(addend[1:])
+        # make negative if needed
+        num = 0 - num if addend[0] == 'L' else num
+        return num
+
+
+dial = Dial()
+rotations = get_rotation_list()
+dial.process_rotation_list(rotations)
+print(dial.times_hit_zero)
